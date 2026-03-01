0113630e39bcee87dca59c8f0dbf61b4c8b9cd77
Ahmed-Ossamaa <aossama2015@gmail.com>
2026-03-01 07:18:12 +0200

    add: Golobal Pipe

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,24 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cookieParser());
   app.enableCors({
     origin: process.env.CLIENT_URL || 'http://localhost:3000',
     credentials: true,
   });
+
+  app.use(cookieParser());
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Task Scheduler API')
     .setDescription('API Documentation for Task Scheduler')
