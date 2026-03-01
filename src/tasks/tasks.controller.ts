65eb043255af50f9c91cd69c5dc0f872e3c61a95
Ahmed-Ossamaa <aossama2015@gmail.com>
2026-03-01 06:48:37 +0200

    Ref: tasks endPoints

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -4,6 +4,7 @@ import {
   Delete,
   Get,
   Param,
+  ParseUUIDPipe,
   Patch,
   Post,
   Query,
@@ -18,7 +19,7 @@ import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { Roles } from 'src/common/decorators/roles.decorator';
 import { UserRole } from 'src/users/enums/user-roles.enum';
-import { ApiBearerAuth } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
 
 @ApiBearerAuth()
 @UseGuards(JwtAuthGuard, RolesGuard)
@@ -27,6 +28,10 @@ export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
   @Post('/schedule')
+  @Roles(UserRole.MANAGER)
+  @ApiOperation({
+    summary: 'Create and Schedule a new task (Manager Only)',
+  })
   async scheduleTask(
     @CurrentUser() user: JwtPayload,
     @Body() taskDto: CreateTaskDTO,
@@ -36,6 +41,9 @@ export class TasksController {
 
   @Get('all')
   @Roles(UserRole.ADMIN)
+  @ApiOperation({
+    summary: 'Get all tasks in the system (Admin Only)',
+  })
   async getAllTasks(
     @Query('page') page: number = 1,
     @Query('limit') limit: number = 20,
@@ -45,33 +53,59 @@ export class TasksController {
 
   @Get('user/:userId')
   @Roles(UserRole.ADMIN, UserRole.MANAGER)
-  async getUserTasks(@Param('userId') userId: string) {
+  @ApiOperation({
+    summary: 'See all tasks assigned to a specific user (Manager & Admin Only)',
+  })
+  async getUserTasks(@Param('userId', ParseUUIDPipe) userId: string) {
     return this.tasksService.getUserTasks(userId);
   }
 
   @Get('/my-tasks')
+  @ApiOperation({
+    summary: 'Get tasks assigned to the current logged-in user',
+  })
   async getMyTasks(@CurrentUser() user: JwtPayload) {
     return this.tasksService.getUserTasks(user.sub);
   }
 
+  @Patch(':taskId/complete')
+  @ApiOperation({
+    summary: 'Mark a task as DONE (Stops the Overdue Timer)',
+  })
+  async completeTask(
+    @CurrentUser() user: JwtPayload,
+    @Param('taskId', ParseUUIDPipe) taskId: string,
+  ) {
+    return this.tasksService.completeTask(taskId, user.sub);
+  }
+
   @Get(':taskId')
+  @ApiOperation({
+    summary: 'Get details of a specific task (tasks of current logged-in user)',
+  })
   async getTaskById(
     @CurrentUser() user: JwtPayload,
-    @Param('taskId') taskId: string,
+    @Param('taskId', ParseUUIDPipe) taskId: string,
   ) {
     return this.tasksService.getTaskById(taskId, user.sub);
   }
 
   @Patch(':taskId')
+  @Roles(UserRole.MANAGER)
+  @ApiOperation({
+    summary: 'Update task details Deadline, Title, etc. (Manager Only)',
+  })
   async updateTask(
     @CurrentUser() user: JwtPayload,
-    @Param('taskId') taskId: string,
+    @Param('taskId', ParseUUIDPipe) taskId: string,
     @Body() taskDto: UpdateTaskDto,
   ) {
     return this.tasksService.updateTask(taskId, taskDto, user.sub);
   }
 
   @Delete(':taskId')
+  @Roles(UserRole.MANAGER, UserRole.ADMIN)
+  @ApiOperation({ summary: 'Delete a task (Manager & Admin Only)' })
   async deleteTask(
     @CurrentUser() user: JwtPayload,
     @Param('taskId') taskId: string,
