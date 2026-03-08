df787e1e45ca146a83f726a60675d195db858d76
Ahmed-Ossamaa <aossama2015@gmail.com>
2026-03-08 08:11:40 +0200

    Add: OrgId to Auth response

--- src/users/dto/user-response.dto.ts
+++ src/users/dto/user-response.dto.ts
@@ -15,6 +15,9 @@ export class UserResponseDto {
   @ApiProperty({ enum: UserRole })
   role: UserRole;
 
+  @ApiPropertyOptional({ example: '123e4567-e89b-12d3-a456-426614174000' })
+  organizationId?: string;
+
   @ApiPropertyOptional({ enum: UserGender })
   gender?: UserGender;
 
