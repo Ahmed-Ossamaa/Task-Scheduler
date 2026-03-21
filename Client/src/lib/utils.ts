9900634b67ddd7e41d1cb95d2655322d666b4f9b
Ahmed-Ossamaa <aossama2015@gmail.com>
2026-03-21 03:11:59 +0200

    helper fn formateDateTime

--- Client/src/lib/utils.ts
+++ Client/src/lib/utils.ts
@@ -4,3 +4,16 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const formatDateTime = (dateString?: string | null) => {
+  if (!dateString) return '-';
+  return new Date(dateString).toLocaleString('en-US', {
+    month: 'short',
+    day: 'numeric',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: '2-digit',
+    hour12: true,
+  });
+};
+
