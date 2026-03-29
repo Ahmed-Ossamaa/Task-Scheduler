30460033cb4e71bc9df8772c92880194c83b2a8a
Ahmed-Ossamaa <aossama2015@gmail.com>
2026-03-29 14:53:16 +0200

    fix: import Storage module into users module

--- Server/src/users/users.module.ts
+++ Server/src/users/users.module.ts
@@ -3,9 +3,10 @@ import { UserService } from './users.service';
 import { UserController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { StorageModule } from 'src/integrations/storage/storage.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), StorageModule],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
