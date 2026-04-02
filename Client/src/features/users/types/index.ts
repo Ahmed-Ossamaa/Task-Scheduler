da75130365ce1cd425586a3e2ed5aeb261d8c940
Ahmed-Ossamaa <aossama2015@gmail.com>
2026-04-02 17:20:12 +0200

    Add: PaginatedUser interface

--- Client/src/features/users/types/index.ts
+++ Client/src/features/users/types/index.ts
@@ -1,6 +1,15 @@
+import { User } from "@/features/auth/types/user-interface";
+
 export interface CreateEmployeeDto {
   name: string;
   gender?: string;
   email: string;
   password: string;
+}
+
+export interface PaginatedUser {
+  data: User[];
+  total: number;
+  page: number;
+  lastPage: number;
 }
