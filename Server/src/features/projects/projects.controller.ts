2edc32d28011531284d24d5a32fa0447040570c2
Ahmed-Ossamaa <aossama2015@gmail.com>
2026-04-11 22:46:24 +0200

    Reorder and clean up Projects endPoints

--- Server/src/features/projects/projects.controller.ts
+++ Server/src/features/projects/projects.controller.ts
@@ -29,6 +29,50 @@ import { UpdateProjectDto } from './dto/update-project.dto';
 export class ProjectsController {
   constructor(private readonly projectsService: ProjectsService) {}
 
+  //-------- GET Routes--------
+
+  @ApiOperation({
+    summary: 'Get all projects for an organization (Manager / Employee)',
+  })
+  @Get('org')
+  @Roles(UserRole.MANAGER, UserRole.EMP)
+  async getOrgProjects(@CurrentUser() user: JwtPayload) {
+    if (!user.organizationId) return [];
+    return this.projectsService.getProjectsByOrg(user.organizationId);
+  }
+
+  @ApiOperation({
+    summary: 'Get deleted projects for an organization (Manager)',
+  })
+  @Get('archived')
+  @Roles(UserRole.MANAGER)
+  async getArchivedProjects(
+    @CurrentUser() manager: JwtPayload,
+    @Query('page', new ParseIntPipe({ optional: true })) page: number = 1,
+    @Query('limit', new ParseIntPipe({ optional: true })) limit: number = 20,
+  ) {
+    if (!manager.organizationId) {
+      throw new ForbiddenException('You are not assigned to an organization.');
+    }
+    return this.projectsService.getDeletedProjects(
+      manager.organizationId,
+      page,
+      limit,
+    );
+  }
+
+  @ApiOperation({ summary: 'Get all projects for all organizations (Admin)' })
+  @Get()
+  @Roles(UserRole.ADMIN)
+  async getAllProjects(
+    @Query('page', new ParseIntPipe({ optional: true })) page: number = 1,
+    @Query('limit', new ParseIntPipe({ optional: true })) limit: number = 20,
+  ) {
+    return this.projectsService.getAllProjects(page, limit);
+  }
+
+  // -------- POST Routes --------
+
   @ApiOperation({ summary: 'Create a project' })
   @Post()
   @Roles(UserRole.MANAGER)
@@ -44,65 +88,7 @@ export class ProjectsController {
     return this.projectsService.createProject(manager.organizationId, dto);
   }
 
-  @ApiOperation({
-    summary: 'Get all projects for an organization (Manager / Employee)',
-  })
-  @Get('org')
-  @Roles(UserRole.MANAGER, UserRole.EMP)
-  async getOrgProjects(@CurrentUser() user: JwtPayload) {
-    if (!user.organizationId) return [];
-    return this.projectsService.getProjectsByOrg(user.organizationId);
-  }
-
-  @ApiOperation({ summary: 'Get all projects for all organizations (Admin)' })
-  @Get()
-  @Roles(UserRole.ADMIN)
-  async getAllProjects(
-    @Query('page', new ParseIntPipe({ optional: true })) page: number = 1,
-    @Query('limit', new ParseIntPipe({ optional: true })) limit: number = 20,
-  ) {
-    return this.projectsService.getAllProjects(page, limit);
-  }
-
-  @ApiOperation({ summary: 'Update a project (Manager)' })
-  @Patch(':projectId')
-  @Roles(UserRole.MANAGER)
-  async updateProject(
-    @CurrentUser() manager: JwtPayload,
-    @Param('projectId', ParseUUIDPipe) projectId: string,
-    @Body() dto: UpdateProjectDto,
-  ) {
-    if (!manager.organizationId) {
-      throw new ForbiddenException(
-        'You must be a manager of an Organization to update a project',
-      );
-    }
-    return this.projectsService.updateProject(
-      projectId,
-      manager.organizationId,
-      dto,
-    );
-  }
-
-  @ApiOperation({
-    summary: 'Soft Delete a project with its associated tasks (Manager)',
-  })
-  @Delete(':projectId/delete')
-  @Roles(UserRole.MANAGER)
-  async deleteProject(
-    @CurrentUser() manager: JwtPayload,
-    @Param('projectId', ParseUUIDPipe) projectId: string,
-  ) {
-    if (!manager.organizationId) {
-      throw new ForbiddenException(
-        'You must be a manager of an Organization to delete a project',
-      );
-    }
-    return this.projectsService.deleteProject(
-      projectId,
-      manager.organizationId,
-    );
-  }
+  // -------- PATCH Routes --------
 
   @ApiOperation({
     summary:
@@ -125,23 +111,45 @@ export class ProjectsController {
     );
   }
 
-  @ApiOperation({
-    summary: 'Get deleted projects for an organization (Manager)',
-  })
-  @Get('archived')
+  @ApiOperation({ summary: 'Update a project (Manager)' })
+  @Patch(':projectId')
   @Roles(UserRole.MANAGER)
-  async getArchivedProjects(
+  async updateProject(
     @CurrentUser() manager: JwtPayload,
-    @Query('page', new ParseIntPipe({ optional: true })) page: number = 1,
-    @Query('limit', new ParseIntPipe({ optional: true })) limit: number = 20,
+    @Param('projectId', ParseUUIDPipe) projectId: string,
+    @Body() dto: UpdateProjectDto,
   ) {
     if (!manager.organizationId) {
-      throw new ForbiddenException('You are not assigned to an organization.');
+      throw new ForbiddenException(
+        'You must be a manager of an Organization to update a project',
+      );
     }
-    return this.projectsService.getDeletedProjects(
+    return this.projectsService.updateProject(
+      projectId,
       manager.organizationId,
-      page,
-      limit,
+      dto,
+    );
+  }
+
+  // -------- DELETE Routes --------
+
+  @ApiOperation({
+    summary: 'Soft Delete a project with its associated tasks (Manager)',
+  })
+  @Delete(':projectId')
+  @Roles(UserRole.MANAGER)
+  async deleteProject(
+    @CurrentUser() manager: JwtPayload,
+    @Param('projectId', ParseUUIDPipe) projectId: string,
+  ) {
+    if (!manager.organizationId) {
+      throw new ForbiddenException(
+        'You must be a manager of an Organization to delete a project',
+      );
+    }
+    return this.projectsService.deleteProject(
+      projectId,
+      manager.organizationId,
     );
   }
 }
