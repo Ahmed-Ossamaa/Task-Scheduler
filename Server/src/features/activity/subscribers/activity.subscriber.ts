fde42e1d1ff1e5ae5521e7f8e49499421c9d7265
Ahmed-Ossamaa <aossama2015@gmail.com>
2026-04-13 17:03:27 +0200

    Add: afterRecover event to activity subscribers

--- Server/src/features/activity/subscribers/activity.subscriber.ts
+++ Server/src/features/activity/subscribers/activity.subscriber.ts
@@ -5,10 +5,15 @@ import {
   RemoveEvent,
   DataSource,
   SoftRemoveEvent,
+  RecoverEvent,
 } from 'typeorm';
 import { Injectable, Logger } from '@nestjs/common';
 import { ActivityLog } from '../entities/activity-log.entity';
-import { ActionType } from '../types/activity-log-types';
+import {
+  ActionType,
+  TrackedEntity,
+  TrackedEvent,
+} from '../types/activity-log-types';
 import { User } from 'src/features/users/entities/user.entity';
 import { Organization } from 'src/features/organizations/entities/organization.entity';
 
@@ -19,89 +24,121 @@ export class ActivitySubscriber implements EntitySubscriberInterface {
   constructor(dataSource: DataSource) {
     dataSource.subscribers.push(this);
   }
-
   //Track Org and Users Tables
   private readonly trackedTables = ['organizations', 'users'];
 
-  async afterInsert(event: InsertEvent<User | Organization>) {
+  /**
+   * Triggered after a new row is inserted into a tracked table (Users, Organizations).
+   * Creates an activity log entry with action type 'CREATED' and details about the inserted row.
+   * @param event The insert event.
+   */
+  async afterInsert(event: InsertEvent<TrackedEntity>) {
     if (!this.trackedTables.includes(event.metadata.tableName)) return;
 
-    const extraInfo = this.getEntityDetails(event.entity);
-    const entityName =
-      event.metadata.tableName === 'organizations' ? 'Organization' : 'User';
-
-    const log = new ActivityLog();
-    log.actionType = ActionType.CREATED;
-    log.entityType = event.metadata.tableName;
-    log.entityId = event.entity.id;
-    log.details = `New ${entityName} created${extraInfo}`;
-
-    await event.manager.save(ActivityLog, log);
+    await this.logActivity(
+      event,
+      ActionType.CREATED,
+      'created',
+      event.entity?.id,
+    );
   }
 
   /**
-   * Triggered after a row is removed from  a table ( Users, Organizations ).
-   * Creates an activity log entry with action type 'DELETED' and details about the deleted row.
+   * @deprecated this  will never be triggered for now
+   * - (No hard delete for now & background job will take care of it using .delete())
    * @param event The remove event.
    */
-  async afterRemove(event: RemoveEvent<User | Organization>) {
+  async afterRemove(event: RemoveEvent<TrackedEntity>) {
     if (!this.trackedTables.includes(event.metadata.tableName)) return;
 
-    let deletedId: string | undefined;
+    await this.logActivity(
+      event,
+      ActionType.DELETED,
+      'permanently deleted by system cleanup',
+      event.entity?.id ||
+        (typeof event.entityId === 'string' ? event.entityId : undefined),
+    );
+  }
 
-    //entity?.id: if there is a fully loaded entity passed (for remove methods)
-    if (event.entity?.id) {
-      deletedId = event.entity.id;
-      //entityId: if there is just an Id passed instead of a fully loaded entity
-    } else if (typeof event.entityId === 'string') {
-      deletedId = event.entityId;
-    }
+  /**
+   * Triggered after a row is soft removed from a table (Users, Organizations).
+   * Creates an activity log entry with action type 'ARCHIVED' and details about the soft removed row.
+   * @param event - The soft remove event.
+   */
+  async afterSoftRemove(event: SoftRemoveEvent<TrackedEntity>) {
+    if (!this.trackedTables.includes(event.metadata.tableName)) return;
 
-    // if there is no Id at all (undefined) or no id due to bulk delete
-    if (!deletedId) {
-      return;
-    }
+    await this.logActivity(
+      event,
+      ActionType.ARCHIVED,
+      'archived',
+      event.entity?.id ||
+        (typeof event.entityId === 'string' ? event.entityId : undefined),
+    );
+  }
+
+  /**
+   * Triggered after a soft removed row is recovered from a table ( Users, Organizations ).
+   * Creates an activity log entry with action type 'RESTORED' and details about the recovered row.
+   * @param event The recover event.
+   */
+  async afterRecover(event: RecoverEvent<TrackedEntity>) {
+    if (!this.trackedTables.includes(event.metadata.tableName)) return;
+
+    await this.logActivity(
+      event,
+      ActionType.RESTORED,
+      'restored',
+      event.entity?.id ||
+        (typeof event.entityId === 'string' ? event.entityId : undefined),
+    );
+  }
+
+  //..........Helper private methods............
+
+  /**
+   * Insterts an activity in the activity log table.
+   * @param {TrackedEvent} event - The event that triggered the logging (insert, remove, etc).
+   * @param {ActionType} actionType - The type of action that was performed.
+   * @param {string} actionText - A descriptive text of the action that was performed.
+   * @param {string} [targetId] - The ID of the entity that was affected by the action.
+   */
+  private async logActivity(
+    event: TrackedEvent,
+    actionType: ActionType,
+    actionText: string,
+    targetId?: string,
+  ) {
+    if (!targetId) return;
 
     const extraInfo = this.getEntityDetails(event.entity);
-
     const entityName =
       event.metadata.tableName === 'organizations' ? 'Organization' : 'User';
 
     const log = new ActivityLog();
-    log.actionType = ActionType.DELETED;
+    log.actionType = actionType;
     log.entityType = event.metadata.tableName;
-    log.entityId = deletedId;
-    log.details = `A ${entityName} was hard deleted${extraInfo}`;
-    await event.manager.save(ActivityLog, log);
-  }
+    log.entityId = targetId;
+    log.details = `${entityName} was ${actionText}${extraInfo}`;
 
-  async afterSoftRemove(event: SoftRemoveEvent<User | Organization>) {
-    if (!this.trackedTables.includes(event.metadata.tableName)) return;
-
-    let deletedId: string | undefined;
-    if (event.entity?.id) deletedId = event.entity.id;
-    else if (typeof event.entityId === 'string') deletedId = event.entityId;
-
-    // if there is no Id at all (undefined) or no id due to bulk delete
-    if (!deletedId) {
-      return;
+    try {
+      await event.manager.save(ActivityLog, log);
+    } catch (error) {
+      this.logger.error(
+        `Failed to save activity log for ${entityName} ${targetId}`,
+        error instanceof Error ? error.stack : error,
+      );
     }
-
-    const extraInfo = this.getEntityDetails(event.entity);
-
-    const entityName =
-      event.metadata.tableName === 'organizations' ? 'Organization' : 'User';
-
-    const log = new ActivityLog();
-    log.actionType = ActionType.ARCHIVED;
-    log.entityType = event.metadata.tableName;
-    log.entityId = deletedId;
-    log.details = `A ${entityName} was soft deleted${extraInfo}`;
-
-    await event.manager.save(ActivityLog, log);
   }
 
-  private getEntityDetails(entity: unknown): string {
+  /**
+   * Returns a string that contains details about the given entity.
+   * - If the entity is a User, returns a string in the format "(name - email)".
+   * - If the entity is an Organization, returns a string in the format "(name)".
+   * - If the entity is null or undefined, returns an empty string.
+   * @param entity The entity to get details from.
+   */
+  private getEntityDetails(entity: TrackedEntity | undefined | null): string {
     if (!entity) return '';
     // If its a User, return email and email
     if (entity instanceof User) {
--- Server/src/features/activity/types/activity-log-types.ts
+++ Server/src/features/activity/types/activity-log-types.ts
@@ -1,6 +1,24 @@
+import { Organization } from 'src/features/organizations/entities/organization.entity';
+import { User } from 'src/features/users/entities/user.entity';
+import {
+  InsertEvent,
+  RecoverEvent,
+  RemoveEvent,
+  SoftRemoveEvent,
+} from 'typeorm';
+
 export enum ActionType {
   CREATED = 'CREATED',
   UPDATED = 'UPDATED',
   DELETED = 'DELETED',
   ARCHIVED = 'ARCHIVED',
+  RESTORED = 'RESTORED',
 }
+
+export type TrackedEntity = User | Organization;
+
+export type TrackedEvent =
+  | InsertEvent<TrackedEntity>
+  | RemoveEvent<TrackedEntity>
+  | SoftRemoveEvent<TrackedEntity>
+  | RecoverEvent<TrackedEntity>;
