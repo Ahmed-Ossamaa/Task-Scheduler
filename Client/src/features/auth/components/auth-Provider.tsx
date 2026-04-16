065333a79b2d6cf558df020eef2cc4898dc657db
Ahmed-Ossamaa <aossama2015@gmail.com>
2026-04-17 01:28:09 +0200

    Ref(auth-provider): read hasSession cookie flag (no api calls to get tokens if its a guest)

--- Client/src/features/auth/components/auth-Provider.tsx
+++ Client/src/features/auth/components/auth-Provider.tsx
@@ -3,20 +3,22 @@
 import { useEffect } from 'react';
 import { useAuthStore } from '@/features/auth/store/auth.store'; 
 import api from '@/lib/api/axios';
+import Cookies from 'js-cookie';
 
 export default function AuthProvider({ children }: { children: React.ReactNode }) {
-  // const setAccessToken = useAuthStore((state) => state.setAccessToken);
   const setUser = useAuthStore((state) => state.setUser);
 
   useEffect(() => {
     const initAuth = async () => {
+      if (!Cookies.get('hasSession')) {
+      return; 
+    }
       try {
         const { data } = await api.get('/user/me'); 
         setUser(data); 
         
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      } catch (error: any) {
-        console.log("Auth: Guest session", error.message);
+      } catch (error) {
+        console.log("Auth: Session expired or invalid" , error);
       }
     };
 
