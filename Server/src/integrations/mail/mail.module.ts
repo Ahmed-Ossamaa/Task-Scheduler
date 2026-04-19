38e7b0182702b2d4c13ef7461e8c032ca782a552
Ahmed-Ossamaa <aossama2015@gmail.com>
2026-04-19 21:07:11 +0200

    Fix(mail/storage): remove configModule from module imports since its initialized globally

--- Server/src/integrations/mail/mail.module.ts
+++ Server/src/integrations/mail/mail.module.ts
@@ -1,11 +1,9 @@
 import { Module, Global } from '@nestjs/common';
 import { MailService } from './mail.interface';
 import { BrevoMailService } from './brevo-mail.service';
-import { ConfigModule } from '@nestjs/config';
 
 @Global()
 @Module({
-  imports: [ConfigModule],
   providers: [
     {
       provide: MailService,
--- Server/src/integrations/storage/storage.module.ts
+++ Server/src/integrations/storage/storage.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { CloudinaryProvider } from './cloudinary.provider';
 import { CloudinaryService } from './cloudinary.service';
 import { StorageService } from './storage.interface';
 
 @Module({
-  imports: [ConfigModule],
   providers: [
     CloudinaryProvider,
     {
