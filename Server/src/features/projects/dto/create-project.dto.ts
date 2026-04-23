a6f7d50f3b5bbf57b19ae2caef0028e8bc2b653a
Ahmed-Ossamaa <aossama2015@gmail.com>
2026-04-23 07:29:53 +0200

    fix(create-project-dto) : Add validation rules (maxLength)

--- Server/src/features/projects/dto/create-project.dto.ts
+++ Server/src/features/projects/dto/create-project.dto.ts
@@ -1,14 +1,17 @@
-import { IsString, IsNotEmpty, IsOptional } from 'class-validator';
+import { IsString, IsNotEmpty, IsOptional, MinLength, MaxLength } from 'class-validator';
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class CreateProjectDto {
   @ApiProperty({ example: 'Skill Up' })
   @IsString()
   @IsNotEmpty()
+  @MinLength(2)
+  @MaxLength(50)
   name: string;
 
   @ApiPropertyOptional({ example: 'Skill Up Project: Learning platform' })
   @IsString()
+  @MaxLength(300)
   @IsOptional()
   description?: string;
 }
