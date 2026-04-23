1560c914ceb2fd127551be9b3f012666ba7cb1b7
Ahmed-Ossamaa <aossama2015@gmail.com>
2026-04-23 05:13:54 +0200

    Add metadata to admin messages & organization and kept page.tsx as server component

--- Client/src/app/dashboard/admin/messages/page.tsx
+++ Client/src/app/dashboard/admin/messages/page.tsx
@@ -1,20 +1,46 @@
-import { AdminMessagesClient } from './admin-messages-client';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { ActiveMessagesList } from '@/features/contact/components/active-messages-list';
+import { ArchivedMessagesList } from '@/features/contact/components/archived-messages-list';
 
 export const metadata = {
   title: 'Messages Center | Admin Dashboard',
   description: 'Manage incoming support requests.',
 };
-
 export default function AdminMessagesPage() {
   return (
-    <div className="flex-1 space-y-6 ">
+    <div className="flex flex-col space-y-6 w-full h-full">
+      {/* page Header */}
       <div>
         <p className="text-muted-foreground mt-2">
           Manage incoming messages and support requests.
         </p>
       </div>
+      <Tabs defaultValue="active" className="flex flex-col w-full">
+        <div className="flex items-center w-full mb-4">
+          <TabsList className="relative grid w-full max-w-100 grid-cols-2 rounded-lg shadow-[0_4px_10px_rgba(120,90,60,0.2),inset_0_1px_2px_rgba(255,255,255,0.4)]">
+            <TabsTrigger
+              value="active"
+              className="border-2 border-transparent data-[state=active]:border-2 data-[state=active]:border-primary data-[state=active]:text-foreground"
+            >
+              Active Inbox
+            </TabsTrigger>
+            <TabsTrigger
+              value="archived"
+              className="border-2 border-transparent data-[state=active]:border-2 data-[state=active]:border-primary data-[state=active]:text-foreground"
+            >
+              Archive
+            </TabsTrigger>
+          </TabsList>
+        </div>
 
-      <AdminMessagesClient />
+        <TabsContent value="active" className="w-full mt-0">
+          <ActiveMessagesList />
+        </TabsContent>
+
+        <TabsContent value="archived" className="w-full mt-0">
+          <ArchivedMessagesList />
+        </TabsContent>
+      </Tabs>
     </div>
   );
 }
