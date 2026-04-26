6dfec779fd25dc7604a1ad10c46ee56d28d8629c
Ahmed-Ossamaa <aossama2015@gmail.com>
2026-04-26 16:37:01 +0300

    Add: sys-setting schema

---
+++ Client/src/lib/schema/system-settings-schema.ts
@@ -0,0 +1,23 @@
+import { z } from 'zod';
+
+export const systemSettingsSchema = z.object({
+  appName: z.string().optional(),
+  contactEmail: z.email('Please enter a valid email address').optional(),
+  contactPhone: z
+    .string()
+    .max(20, 'Phone number must be at most 20 characters')
+    .optional(),
+  contactCityAddress: z.string().optional(),
+  contactStreetAddress: z.string().optional(),
+  logo: z.any().optional(),
+  landingPageImage: z.any().optional(),
+  banner: z.string().optional(),
+  facebookUrl: z.string().optional(),
+  twitterUrl: z.string().optional(),
+  instagramUrl: z.string().optional(),
+  youtubeUrl: z.string().optional(),
+  ticktokUrl: z.string().optional(),
+  linkedinUrl: z.string().optional(),
+});
+
+export type SystemSettingsValues = z.infer<typeof systemSettingsSchema>;
