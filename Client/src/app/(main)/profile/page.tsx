435f0a9d6c9f7c6ed75de8f5a1b0122c024cc721
Ahmed-Ossamaa <aossama2015@gmail.com>
2026-04-27 02:12:08 +0300

    Rename(public) directory to (main) and moved profile page into it & let the layout handle the nav/footer for it

--- Client/src/app/(main)/profile/page.tsx
+++ Client/src/app/(main)/profile/page.tsx
@@ -1,6 +1,4 @@
-import { Navbar } from '@/components/layout/navbar';
-import { ProfileClient } from '../../features/users/components/user-profile';
-import { Footer } from '@/components/layout/footer';
+import { ProfileClient } from '../../../features/users/components/user-profile';
 
 export const metadata = {
   title: 'Profile | Task Flow',
@@ -9,12 +7,8 @@ export const metadata = {
 
 export default function ProjectsPage() {
   return (
-    <>
-      <Navbar />
       <div className="max-w-6xl mx-auto p-4 md:p-8 w-full mt-10 mb-10">
         <ProfileClient />
       </div>
-      <Footer />
-    </>
   );
 }
