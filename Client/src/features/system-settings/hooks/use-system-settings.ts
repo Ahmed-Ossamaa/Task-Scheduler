2c17439a1d54485d4e385a1d5e36bb78095c142d
Ahmed-Ossamaa <aossama2015@gmail.com>
2026-04-27 10:05:25 +0300

    Fix(sys-settings: hooks , force refresh after mutation

--- Client/src/features/system-settings/hooks/use-system-settings.ts
+++ Client/src/features/system-settings/hooks/use-system-settings.ts
@@ -1,5 +1,6 @@
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { systemSettingsApi } from '../api/system-settings.api';
+import { useRouter } from 'next/navigation';
 
 export const useGetSystemSettings = () => {
   return useQuery({
@@ -10,42 +11,52 @@ export const useGetSystemSettings = () => {
 
 export const useUpdateSystemSettings = () => {
   const queryClient = useQueryClient();
+  const router = useRouter();
 
   return useMutation({
     mutationFn: systemSettingsApi.updateSettings,
+
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['system-settings'] });
+      router.refresh();
     },
   });
 };
 
 export const useRestoreSystemSettings = () => {
   const queryClient = useQueryClient();
+  const router = useRouter();
 
   return useMutation({
     mutationFn: systemSettingsApi.restoreDefaults,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['system-settings'] });
+      router.refresh();
     },
   });
 };
 
 export const useUploadLogo = () => {
   const queryClient = useQueryClient();
+  const router = useRouter();
   return useMutation({
     mutationFn: systemSettingsApi.uploadLogo,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['system-settings'] });
+      router.refresh();
     },
   });
 };
 
 export const useUploadLandingImage = () => {
   const queryClient = useQueryClient();
+  const router = useRouter();
+
   return useMutation({
     mutationFn: systemSettingsApi.uploadLandingImage,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['system-settings'] });
+      router.refresh();
     },
   });
 };
