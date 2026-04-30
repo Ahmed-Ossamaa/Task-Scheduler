a9b308ccfb29a4414249144abc634bccc985103e
Ahmed-Ossamaa <aossama2015@gmail.com>
2026-04-30 08:01:03 +0300

    Fix(auth-provider):Add ready state to block UI until auth init is done

--- Client/src/features/auth/components/auth-Provider.tsx
+++ Client/src/features/auth/components/auth-Provider.tsx
@@ -1,29 +1,54 @@
 'use client';
 
-import { useEffect } from 'react';
-import { useAuthStore } from '@/features/auth/store/auth.store'; 
+import { useEffect, useState } from 'react';
+import { useAuthStore } from '@/features/auth/store/auth.store';
 import api from '@/lib/api/axios';
+import axios from 'axios';
 import Cookies from 'js-cookie';
+import { setAccessToken, clearAuth } from '@/features/auth/store/auth.store';
 
-export default function AuthProvider({ children }: { children: React.ReactNode }) {
+export default function AuthProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const setUser = useAuthStore((state) => state.setUser);
+  const [ready, setReady] = useState(false);
 
   useEffect(() => {
     const initAuth = async () => {
+      // if user has no session (skip api calls)
       if (!Cookies.get('hasSession')) {
-      return; 
-    }
+        setReady(true);
+        return;
+      }
+
       try {
-        const { data } = await api.get('/user/me'); 
-        setUser(data); 
-        
+        //  Get fresh access token  1st
+        const refreshRes = await axios.post(
+          `${process.env.NEXT_PUBLIC_API_URL}/auth/refresh`,
+          {},
+          { withCredentials: true },
+        );
+
+        const accessToken = refreshRes.data.accessToken;
+        setAccessToken(accessToken);
+
+        // call protected endpoint with  access token
+        const { data } = await api.get('/user/me');
+        setUser(data);
       } catch (error) {
         console.log("Auth: Session expired or invalid" , error);
+        clearAuth();
+      } finally {
+        setReady(true);
       }
     };
 
     initAuth();
   }, [setUser]);
+
+  if (!ready) return null;
 
   return <>{children}</>;
 }
